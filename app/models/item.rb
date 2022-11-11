c2d9c7a2f5cb9d7275b8168998e6f057476ee13f
nintaro02 <g.z.m7968@gmail.com>
2022-11-11 23:14:37 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,17 +9,18 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :status
   belongs_to :category
-  
-  validates :user, presence: true
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "Half-width number" }, 
-  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+
+  validates :product,presence:true
+  validates :description,presence:true
+  validates :image, presence:true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
   
   with_options numericality: { other_than: 0, message: "can't be blank" } do
-  validates :status_id, presence: true 
-  validates :postage_id, presence: true 
-  validates :shipping_day_id, presence: true
-  validates :prefecture_id, presence: true 
-  validates :category_id, presence: true
+  validates :status_id 
+  validates :postage_id 
+  validates :shipping_day_id
+  validates :prefecture_id 
+  validates :category_id
   end
 end
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe Item, type: :model do
       end
       context '出品できない時' do
       it '商品画像が空だと出品できない' do
-        @item.user = nil
+        @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User can't be blank")
+        expect(@item.errors.full_messages).to include("Image can't be blank")
         end
       it '商品名が空だと出品できない' do
         @item.product = ''
@@ -27,27 +27,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリーが未選択だと出品できない' do
-        @item.category_id = ''
+        @item.category_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '状態が未選択だと出品できない' do
-        @item.status_id = ''
+        @item.status_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it '配送料の負担が未選択だと出品できない' do
-        @item.postage_id = ''
+        @item.postage_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Postage can't be blank")
       end
       it '発送地域が未選択だと出品できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数が未選択だと出品できない' do
-        @item.shipping_day_id = ''
+        @item.shipping_day_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
@@ -61,6 +61,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
+      it '300未満の値では保存できないこと' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
     end
+    it '10000000以上の値では保存できないこと' do
+      @item.price = '20000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price Out of setting range')
   end
+end
+end
 end
