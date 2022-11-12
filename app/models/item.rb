a7c722bc680a80968463f41d1b8f133b4086f363
nintaro02 <g.z.m7968@gmail.com>
2022-11-12 14:34:19 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,6 @@ class Item < ApplicationRecord
   validates :product, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  validates :user, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe Item, type: :model do
       it 'userが空だと登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User can't be blank")
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
