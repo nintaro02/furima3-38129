ce45346baf4e43e86b53cdc67aeb4add14c58450
nintaro02 <g.z.m7968@gmail.com>
2022-11-14 21:32:02 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -42,5 +42,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product, :price, :description, :status_id, :postage_id, :shipping_day_id, :prefecture_id,
                                  :category_id, :image, :user).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
