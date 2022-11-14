10c0010a29eea4ace11a363c6f393c9a0d9e93fb
nintaro02 <g.z.m7968@gmail.com>
2022-11-14 21:07:35 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -18,6 +18,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path if @item.user_id != current_user.id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
@@ -26,6 +41,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:product, :price, :description, :status_id, :postage_id, :shipping_day_id, :prefecture_id,
-                                 :category_id, :image).merge(user_id: current_user.id)
+                                 :category_id, :image, :user).merge(user_id: current_user.id)
   end
 end
